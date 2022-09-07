f7793bcc41ca5a08a21ec188bf0be98f77a4bcbf
Adam Allport <adam@allport.me.uk>
2022-09-07 21:32:48 +0100

    feat: Queues

--- app/Console/Commands/PrinterTest.php
+++ app/Console/Commands/PrinterTest.php
@@ -2,14 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\PrintJob;
 use App\Utils\LinePrint\LinePrint;
 use App\Utils\LinePrint\Sections\LineSection;
 use App\Utils\LinePrint\Sections\LineSectionSpan;
 use App\Utils\LinePrint\Sections\SectionSettings;
 use App\Utils\LinePrint\Sections\TextSectionSettings;
 use Illuminate\Console\Command;
-use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
-use Mike42\Escpos\Printer;
 
 class PrinterTest extends Command
 {
@@ -24,21 +23,20 @@ class PrinterTest extends Command
      */
     public function handle()
     {
-        $printer = new Printer(new NetworkPrintConnector("192.168.20.10"));
-
-        LinePrint::make()
-            ->line("PER-001", fn(TextSectionSettings $s) => $s
-                ->textSize(2, 2)
-                ->align(TextSectionSettings::ALIGN_CENTER))
-            ->line("Left")
-            ->line("Center", fn(TextSectionSettings $s) => $s->align(TextSectionSettings::ALIGN_CENTER))
-            ->line("Right", fn(TextSectionSettings $s) => $s->align(TextSectionSettings::ALIGN_RIGHT))
-            ->line(fn(LineSection $line) => $line
-                ->text("None")
-                ->text("Single", fn(LineSectionSpan $s) => $s->underline())
-                ->text("Double", fn(LineSectionSpan $s) => $s->underline(LineSectionSpan::UNDERLINE_DOUBLE))
-            )->qrCode("https://youtu.be/dQw4w9WgXcQ", fn(SectionSettings $s) => $s->align(TextSectionSettings::ALIGN_CENTER))
-            ->render($printer);
+        PrintJob::dispatchSync(
+            LinePrint::make()
+                ->line("PER-001", fn(TextSectionSettings $s) => $s
+                    ->textSize(2, 2)
+                    ->align(TextSectionSettings::ALIGN_CENTER))
+                ->line("Left")
+                ->line("Center", fn(TextSectionSettings $s) => $s->align(TextSectionSettings::ALIGN_CENTER))
+                ->line("Right", fn(TextSectionSettings $s) => $s->align(TextSectionSettings::ALIGN_RIGHT))
+                ->line(fn(LineSection $line) => $line
+                    ->text("None")
+                    ->text("Single", fn(LineSectionSpan $s) => $s->underline())
+                    ->text("Double", fn(LineSectionSpan $s) => $s->underline(LineSectionSpan::UNDERLINE_DOUBLE))
+                )->qrCode("https://youtu.be/dQw4w9WgXcQ", fn(SectionSettings $s) => $s->align(TextSectionSettings::ALIGN_CENTER))
+        );
 
         return 1;
     }
